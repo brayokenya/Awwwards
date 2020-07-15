d0473cdb2790c60ac8377870cabaeb621b1c16e3
brayokenya <kiirubrian21@gmail.com>
2020-07-15 15:16:21 +0300

    heroku deployment

--- awwwards/wsgi.py
+++ awwwards/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'awwwards.settings.dev')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'awwwards.settings')
 
 application = get_wsgi_application()
